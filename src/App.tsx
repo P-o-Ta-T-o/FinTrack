550573fbeb5cd098f683f699449dc584e5679943
P-o-Ta-T-o <swetank.rao.sr@gmail.com>
2024-06-17 23:11:59 +0530

    signed in as 1st user

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Dashboard } from './pages/dash';
+import { Auth } from './pages/auth';
 
 function App() {
-  return <></>;
+  return <Router>
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/auth" element={<Auth/>}/>
+      </Routes>
+    </div>
+  </Router >;
 }
 
 export default App
