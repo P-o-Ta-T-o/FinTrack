1d91a7085c599bb0ec60427be0f2b9f8f76630f3
P-o-Ta-T-o <swetank.rao.sr@gmail.com>
2024-06-18 22:18:00 +0530

    tables formed

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,13 @@ import express, {Express} from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import financialRecordModel from "./schema/financial-record";
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 const mongoURI: string = "mongodb+srv://f20220434:wEDab1jCw5OuYzBz@fintrackwebapp.opcvomr.mongodb.net/";
 
